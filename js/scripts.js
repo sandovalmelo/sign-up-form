31d96a6fb5a45da9aaedded79be67a8a75928615
Sandoval Melo <juniormelo777@gmail.com>
2022-07-06 17:35:36 -0300

    add success message to form submit

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,7 @@ const lastName = document.getElementById("last-name");
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const submitButton = document.getElementById("submit-button");
+const signUpTerms = document.querySelector(".sign-up-terms");
 
 function generateErrorFeedBack(element, message) {
 	const errorMessage = document.createElement("p");
@@ -17,23 +18,32 @@ function generateErrorFeedBack(element, message) {
 function validateEmail(email) {
 	if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
 		return true;
+	} else {
+		return false;
 	}
-
-	return false;
 }
 
 form.addEventListener("submit", (event) => {
 	event.preventDefault();
+	let isFirstNameValid;
+	let isLastNameValid;
+	let isEmailValid = validateEmail(email.value);
 
 	if (!firstName.value) {
 		generateErrorFeedBack(firstName, "First name can not be empty");
+		isFirstNameValid = false;
+	} else {
+		isFirstNameValid = true;
 	}
 
 	if (!lastName.value) {
 		generateErrorFeedBack(lastName, "Last name can not be empty");
+		isLastNameValid = false;
+	} else {
+		isLastNameValid = true;
 	}
 
-	if (!validateEmail(email.value)) {
+	if (!isEmailValid) {
 		generateErrorFeedBack(email, "Looks like this is not an email");
 	} else {
 		email.value = "";
@@ -41,19 +51,34 @@ form.addEventListener("submit", (event) => {
 
 	if (!password.value) {
 		generateErrorFeedBack(password, "Password can not be empty");
+		isPasswordValid = false;
+	} else {
+		isPasswordValid = true;
 	}
+
+	submitButton.disabled = true;
 
 	firstName.value = "";
 	lastName.value = "";
-
 	password.value = "";
 
-	submitButton.disabled = true;
+	let successMessage;
+
+	if (isFirstNameValid && isLastNameValid && isEmailValid && isPasswordValid) {
+		signUpTerms.hidden = true;
+		successMessage = document.createElement("p");
+		successMessage.classList.add("success-message");
+		successMessage.innerText = "Thank you for filling out our sign up form.";
+		form.appendChild(successMessage);
+	}
+
 	const errorMessages = document.querySelectorAll(".error-message");
 
 	setTimeout(() => {
 		formGroup.forEach((group) => group.classList.remove("error"));
 		errorMessages.forEach((message) => message.remove());
+		successMessage ? successMessage.remove() : null;
 		submitButton.disabled = false;
+		signUpTerms.hidden = false;
 	}, 2000);
 });
